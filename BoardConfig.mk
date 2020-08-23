27f036ce87224344cb7b30b1933fff36c3fe6639
Julian Veit <claymore1298@gmail.com>
2020-08-23 14:52:01 +0200

    enchilada: switch to AOSP recovery
    Change-Id: Ic752190994dca81948e06838490d7e0b818172a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # Kernel
 TARGET_KERNEL_CONFIG := enchilada_defconfig
 
-#TWRP Support
--include device/twrp/enchilada/twrp.mk
+# Recovery
+TARGET_USES_AOSP_RECOVERY := true
 
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
